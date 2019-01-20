7b1c8ccf1b3c4a41f9644d195be89d78c7e2ae21
TihomirMihaylov <tihomir_mihaylov@abv.bg>
2019-01-20 14:35:39 +0200

    Circular Queue implementation

---
+++ Circular-Queue/CircularQueue/CircularQueue.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+public class CircularQueue<T>
+{
+    private const int DefaultCapacity = 4;
+    private T[] elements;
+    private int startIndex; //позицията на първия добавен елемент
+    private int endIndex; // позицията, на която ще бъде добавен следващия елемент
+
+    public int Count { get; private set; }
+
+    public CircularQueue(int capacity = DefaultCapacity)
+    {
+        this.elements = new T[capacity];
+    }
+
+    public void Enqueue(T element)
+    {
+        if (this.Count >= this.elements.Length)
+        {
+            this.IncreaseSize();
+        }
+
+        this.elements[this.endIndex] = element;
+        this.endIndex = (this.endIndex + 1) % this.elements.Length; //остатъка от това деление ще превърти дължината на масива и ще ни върне на 0-левия елемент 
+        this.Count++;
+    }
+
+    public T Dequeue()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException("The queue is empty!");
+        }
+        if(this.Count <= this.elements.Length / 4)
+        {
+            this.DecreaseSize();
+        }
+
+        T result = this.elements[this.startIndex];
+        this.startIndex = (this.startIndex + 1) % this.elements.Length;
+        this.Count--;
+
+        return result;
+    }
+
+    public T[] ToArray()
+    {
+        T[] newArray = new T[this.Count];
+        this.CopyAllElements(newArray);
+        return newArray;
+    }
+
+    private void IncreaseSize()
+    {
+        T[] newArray = new T[this.elements.Length * 2];
+
+        this.CopyAllElements(newArray);
+        this.elements = newArray;
+        this.startIndex = 0;
+        this.endIndex = this.Count;
+    }
+
+    private void DecreaseSize()
+    {
+        T[] newArray = new T[this.elements.Length / 2];
+
+        this.CopyAllElements(newArray);
+        this.elements = newArray;
+        this.startIndex = 0;
+        this.endIndex = this.Count;
+    }
+
+    private void CopyAllElements(T[] newArray)
+    {
+        int sourceIndex = this.startIndex;
+        int destinationIndex = 0;
+
+        while (destinationIndex < this.Count)
+        {
+            newArray[destinationIndex++] = this.elements[sourceIndex];
+            sourceIndex = (sourceIndex + 1) % this.elements.Length;
+        }
+    }
+}
+
+
+public class Example
+{
+    public static void Main()
+    {
+
+        CircularQueue<int> queue = new CircularQueue<int>();
+
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        queue.Enqueue(4);
+        queue.Enqueue(5);
+        queue.Enqueue(6);
+
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        int first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        queue.Enqueue(-7);
+        queue.Enqueue(-8);
+        queue.Enqueue(-9);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        queue.Enqueue(-10);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+    }
+}
