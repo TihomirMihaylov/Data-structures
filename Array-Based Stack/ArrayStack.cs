3acd4da37d514d3a5f21a9122271bef501eb6426
TihomirMihaylov <tihomir_mihaylov@abv.bg>
2019-01-20 15:04:34 +0200

    Array-Based Stack implementation

---
+++ Array-Based Stack/ArrayStack.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ArrayStack<T> : IEnumerable<T>
+{
+    private const int InitialCapacity = 16;
+
+    private T[] elements;
+
+    public int Count { get; private set; }
+
+    public ArrayStack(int capacity = InitialCapacity)
+    {
+        this.elements = new T[capacity];
+    }
+
+    public void Push(T element)
+    {
+        if (this.Count == this.elements.Length)
+        {
+            this.Grow();
+        }
+
+        this.elements[this.Count++] = element;
+    }
+
+    public T Pop()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        this.Count--;
+        return this.elements[this.Count];
+    }
+
+    public T[] ToArray()
+    {
+        T[] array = new T[this.Count];
+        for (int i = 0; i < this.Count; i++)
+        {
+            array[i] = this.elements[this.Count - 1 - i];
+        }
+
+        return array;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = this.Count -1; i >= 0; i--)
+        {
+            yield return this.elements[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    private void Grow()
+    {
+        T[] newElements = new T[this.elements.Length * 2];
+        for (int i = 0; i < this.Count; i++)
+        {
+            newElements[i] = this.elements[i];
+        }
+
+        this.elements = newElements;
+    }
+}
---
+++ Array-Based Stack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayStack<int> stack = new ArrayStack<int>();
+        stack.Push(2);
+        stack.Push(4);
+        stack.Push(-5);
+        stack.Push(8);
+        Console.WriteLine(stack.Count);
+        Console.WriteLine(String.Join(" ", stack.ToArray()));
+        Console.WriteLine();
+
+        int popped = stack.Pop();
+        Console.WriteLine(popped);
+        Console.WriteLine(stack.Count);
+        Console.WriteLine(String.Join(" ", stack.ToArray()));
+        Console.WriteLine();
+
+        foreach (var item in stack)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
