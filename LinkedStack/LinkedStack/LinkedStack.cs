1bd8d93e81a172cd653e242cb4c651bf1f2919fc
TihomirMihaylov <tihomir_mihaylov@abv.bg>
2019-01-20 14:29:55 +0200

    Linked Stack implementation

---
+++ LinkedStack/LinkedStack/LinkedStack.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LinkedStack
+{
+    public class LinkedStack<T> : IEnumerable<T>
+    {
+        private Node firstNode;
+
+        public int Count { get; private set; }
+
+        public void Push(T element) // == AddLast()
+        {
+            this.firstNode = new Node(element, this.firstNode); //движим се отгоре надолу в смисъл следващия е долния
+
+            this.Count++;
+        }
+
+        public T Pop() // == RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            T result = this.firstNode.Value;
+            this.firstNode = this.firstNode.NextNode; // върха на стека става следващия елемент на досегашния връх
+            this.Count--;
+
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.firstNode.Value;
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[this.Count];
+
+            int index = 0;
+            Node current = this.firstNode;
+            while (current != null)
+            {
+                array[index++] = current.Value;
+                current = current.NextNode;
+            }
+
+            return array;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = this.firstNode;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.NextNode;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class Node
+        {
+            public Node(T value, Node nextNode = null)
+            {
+                this.Value = value;
+                this.NextNode = nextNode;
+            }
+
+            public T Value { get; set; }
+            public Node NextNode { get; set; }
+        }
+    }
+}
---
+++ LinkedStack/LinkedStack/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LinkedStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedStack<int> stack = new LinkedStack<int>();
+            stack.Push(-10);
+            stack.Push(5);
+            stack.Push(0);
+            stack.Push(2);
+            stack.Pop();
+
+            foreach (var item in stack)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
