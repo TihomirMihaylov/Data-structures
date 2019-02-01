c726a1e21c5c9cd0b91be81f8467851735a3f12c
TihomirMihaylov <tihomir_mihaylov@abv.bg>
2019-02-01 16:39:51 +0200

    Binary Tree implementation with Pre-order, In-order and Post-order traversals

---
+++ BinaryTree/BinaryTree/BinaryTree.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    public class BinaryTree<T>
+    {
+        public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+        {
+            this.Value = value;
+            this.LeftChild = leftChild;
+            this.RightChild = rightChild;
+        }
+
+        public T Value { get; set; }
+
+        public BinaryTree<T> LeftChild { get; set; }
+
+        public BinaryTree<T> RightChild { get; set; }
+
+        //Root -> Left -> Right
+        public void PrintIndentedPreOrder(int indent = 0)
+        {
+            Console.WriteLine(new string(' ', indent) + this.Value); //принтираме текущия елемент
+
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.PrintIndentedPreOrder(indent + 2);
+            }
+            if (this.RightChild != null)
+            {
+                this.RightChild.PrintIndentedPreOrder(indent + 2);
+            }
+        }
+
+        //Left -> Root -> Right
+        //може и направя и по горния образец!
+        public void EachInOrder(Action<T> action) // Action<T> - делегат; референция към метод. На един метод можем да подадем друг метод
+        {
+            this.EachInOrder(this, action);
+        }
+
+        //Left -> Right -> Root
+        public void EachPostOrder(Action<T> action)
+        {
+            EachPostOrder(this, action);
+        }
+
+        //правим допълнителен helper метод, който всъщност е рекурсията, а горния само го извиква
+        private void EachInOrder(BinaryTree<T> node, Action<T> action) //вече имаме референция към това, което искаме да проверяваме дали е null
+        {
+            if (node == null) //тази проверка ще ни предпази от Null reference exception; ще ни изкара от рекурсията.
+            {
+                return;
+            }
+
+            EachInOrder(node.LeftChild, action);
+            action(node.Value);
+            EachInOrder(node.RightChild, action);
+        }
+
+        private void EachPostOrder(BinaryTree<T> node, Action<T> action)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            EachPostOrder(node.LeftChild, action);
+            EachPostOrder(node.RightChild, action);
+            action(node.Value);
+        }
+    }
+
+}
---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree<int> bt =
+            new BinaryTree<int>(5,
+                leftChild: new BinaryTree<int>(2),
+                rightChild: new BinaryTree<int>(1));
+
+            bt.PrintIndentedPreOrder();
+            Console.WriteLine();
+            bt.EachInOrder(x => Console.WriteLine(x));
+            Console.WriteLine();
+            bt.EachPostOrder(x => Console.WriteLine(x));
+            Console.WriteLine("_____________________________________");
+
+            var binaryTree =
+                new BinaryTree<string>("*",
+                    new BinaryTree<string>("+",
+                        new BinaryTree<string>("3"),
+                        new BinaryTree<string>("2")),
+                    new BinaryTree<string>("-",
+                        new BinaryTree<string>("9"),
+                        new BinaryTree<string>("6")));
+
+            Console.WriteLine("Binary tree (indented, pre-order):");
+            binaryTree.PrintIndentedPreOrder();
+
+            Console.Write("Binary tree nodes (in-order):");
+            binaryTree.EachInOrder(c => Console.Write(" " + c));
+            Console.WriteLine();
+
+            Console.Write("Binary tree nodes (post-order):");
+            binaryTree.EachPostOrder(c => Console.Write(" " + c));
+            Console.WriteLine();
+        }
+    }
+}
