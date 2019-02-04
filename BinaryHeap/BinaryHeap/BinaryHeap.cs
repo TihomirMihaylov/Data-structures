8d5ed65410a0373151053af1655dcc738cf93067
TihomirMihaylov <tihomir_mihaylov@abv.bg>
2019-02-04 21:51:35 +0200

    Binary Heap (Max) implementation and Heap Sort algorithm

---
+++ BinaryHeap/BinaryHeap/BinaryHeap.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+//MAX HEAP
+public class BinaryHeap<T> where T : IComparable<T>
+{
+    private List<T> heap;
+
+    public BinaryHeap()
+    {
+        this.heap = new List<T>();
+    }
+
+    public int Count => this.heap.Count;
+
+    public void Insert(T item)
+    {
+        this.heap.Add(item);
+        this.HeapifyUp(this.heap.Count - 1); //индекса на елемента, който сме добавили.
+    }
+
+    public T Peek()
+    {
+        if(this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return this.heap[0];
+    }
+
+    public T Pull()
+    {
+        T deletedElement = this.Peek(); //включва проверката за Count == null
+        this.Swap(0, this.Count - 1);
+        this.heap.RemoveAt(this.Count - 1);
+        this.HeapifyDown(0);
+
+        return deletedElement;
+    }
+
+    private void HeapifyUp(int index)
+    {
+        int parent = (index - 1) / 2;
+        while (this.heap[index].CompareTo(this.heap[parent]) > 0)
+        {
+            Swap(index, parent);
+            index = parent;
+            parent = (index - 1) / 2;
+        }
+    }
+
+    private void Swap(int a, int b)
+    {
+        T temp = this.heap[a];
+        this.heap[a] = this.heap[b];
+        this.heap[b] = temp;
+    }
+
+    private void HeapifyDown(int index)
+    {
+        while (index < this.Count / 2) //бутаме го надолу докато индекса стане по-малък от половината на брой елементи защото елемента по средата със сигурност няма деца; щом няма деца значи е най-долу;
+        {
+            int child = 2 * index + 1;
+            bool rightChildExists = child + 1 < this.Count;
+            if(rightChildExists && this.heap[child + 1].CompareTo(this.heap[child]) > 0)
+            {
+                child++;
+            }
+            if(this.heap[index].CompareTo(this.heap[child]) > 0)
+            {
+                break;
+            }
+
+            this.Swap(index, child);
+            index = child;
+        }
+    }
+}
---
+++ BinaryHeap/BinaryHeap/Heap.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public static class Heap<T> where T : IComparable<T>
+{
+    public static void Sort(T[] arr) //сложността е Nlog(N)
+    {
+        //1)конструираме heap-a
+        for (int i = arr.Length / 2; i >= 0; i--)
+        {
+            HeapifyDown(arr, i, arr.Length);
+        }
+        //2)
+        for (int i = arr.Length - 1; i > 0; i--)
+        {
+            Swap(arr, 0, i);  //swap-ваме първия и последния
+            HeapifyDown(arr, 0, i);
+        }
+    }
+
+    private static void HeapifyDown(T[] arr, int index, int length)
+    {
+        while (index < length / 2)
+        {
+            int child = 2 * index + 1;
+            bool rightChildExists = child + 1 < length;
+            if(rightChildExists && arr[child + 1].CompareTo(arr[child]) > 0)
+            {
+                child++;
+            }
+            if(arr[index].CompareTo(arr[child]) > 0)
+            {
+                break;
+            }
+
+            Swap(arr, index, child);
+            index = child;
+        }
+    }
+
+    private static void Swap(T[] arr, int a, int b)
+    {
+        T temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+}
